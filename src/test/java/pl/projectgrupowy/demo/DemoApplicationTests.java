e2674e7c9fe8dee6e781ca7848cd0c997bd60a09
Jacek Jozefczuk <jjoze@go2.pl>
2020-09-26 12:33:34 +0200

    init

---
+++ src/test/java/pl/projectgrupowy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projectgrupowy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
